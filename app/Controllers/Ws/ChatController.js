a1ca4f1c6132f4bb9376b4e720635512855dd553
thiagofariasdev <thiago_fdutra@hotmail.com>
2019-03-14 09:11:05 -0300

    changes

--- app/Controllers/Ws/ChatController.js
+++ app/Controllers/Ws/ChatController.js
@@ -1,11 +1,11 @@
 'use strict'
 
 class ChatController {
-  constructor ({ socket, request }) {
-    this.socket = socket
-    this.request = request
-  }
-  
+	constructor({ socket, request }) {
+		this.socket = socket
+		this.request = request
+	}
+
 }
 
 module.exports = ChatController
