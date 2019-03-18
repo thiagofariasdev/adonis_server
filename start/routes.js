a7e7e5eb375ba2023b2c2d3e32ae387a4b3edcc4
Thiago Farias <thiago_fdutra@hotmail.com>
2019-03-17 22:48:55 -0300

    changes

--- start/routes.js
+++ start/routes.js
@@ -15,18 +15,36 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
+const c_msg = [];
 
 Route.on('/').render('welcome');
 Route.on('/login').render('login');
 Route.on('/register').render('register');
 Route.on('/app').render('app');
+
+// Auth WEB
 Route.group(function () {
     Route.get('/chats', async () => { return render('app') });
     Route.on('/contacts', async () => { return render('app') });
     Route.on('/profile/:id?', 'UserController.profile');
     Route.get('/search', 'DataController.find');
 }).middleware(['auth'])
+
+//API
 Route.group(function () {
     Route.post('login', 'UserController.login');
     Route.post('register', 'UserController.store');
-}).prefix('api')
+    Route.get('/send-message', ({ response }) => {
+        c_msg.push('Hello\n\n');
+        response.send('OK: ' + c_msg);
+    })
+    Route.get('/messages', ({ response }) => {
+        response.header('Content-type', 'text/event-stream');
+        response.send(c_msg);
+    })
+}).prefix('api');
+
+//Auth API
+Route.group(function () {
+
+}).prefix('api').middleware('auth');
